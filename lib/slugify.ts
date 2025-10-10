41318b0ebe5d16998afd9b7277dc2c2ef8d058c4
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-10-10 23:44:39 +0200

    Create slugify.ts

---
+++ lib/slugify.ts
@@ -0,0 +1,10 @@
+// lib/slugify.ts
+export function slugify(input: string): string {
+  return input
+    .normalize("NFKD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+|-+$/g, "")
+    .replace(/-{2,}/g, "-");
+}
